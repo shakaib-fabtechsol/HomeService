b4d43cb33bb753784b5524f79f1599978bd1ecd5
shakaib-fabtechsol <shakaib@fabtechsol.com>
2025-01-29 17:57:56 +0500

    payment tab on last

--- src/Layouts/Admin/Settings.jsx
+++ src/Layouts/Admin/Settings.jsx
@@ -199,11 +199,11 @@ function Settings() {
             <Tab label="Location" />
             <Tab label="Business Profile " />
             <Tab label="Certifications & Hours" />
-            <Tab label="Payment/Payout Info" />
             <Tab label="Additional Photos" />
             <Tab label="Social Profiles" />
             <Tab label="Password" />
             <Tab label="Channels for Conversations" />
+            <Tab label="Payment/Payout Info" />
           </Tabs>
         </Box>
 
@@ -854,123 +854,6 @@ function Settings() {
           <div>
             <div className="border-b border-[#E9EAEB] pb-5 items-center flex-wrap gap-4">
               <p className="text-lg font-semibold text-[#181D27]">
-                Payment/Payout Info
-              </p>
-              <p className="text-[#535862] text-sm">
-                update your payment detail
-              </p>
-            </div>
-            <div className="">
-              <div className="grid border-b py-4 grid-cols-1 md:grid-cols-12">
-                <div className="col-span-12 md:col-span-4 lg:col-span-3">
-                  <div className="flex">
-                    <label htmlFor="Title" className="font-semibold">
-                      Service Title
-                    </label>
-                  </div>
-                </div>
-                <div className="col-span-12 md:col-span-8 lg:col-span-6">
-                  <input
-                    type="text"
-                    id="Title"
-                    placeholder="Muhammad Hussnain"
-                    className="myinput focus-none w-full"
-                  />
-                </div>
-              </div>
-              <div className="grid border-b py-4 grid-cols-1 md:grid-cols-12">
-                <div className="col-span-12 md:col-span-4 lg:col-span-3">
-                  <div className="flex">
-                    <label htmlFor="Title" className="font-semibold">
-                      Select Bank*
-                    </label>
-                  </div>
-                </div>
-                <div className="col-span-12 md:col-span-8 lg:col-span-6">
-                  <select id="Bank" className="myinput focus-none w-full">
-                    <option value="" disabled selected hidden>
-                      Select a bank
-                    </option>
-                    <option value="bank1">Bank 1</option>
-                    <option value="bank2">Bank 2</option>
-                    <option value="bank3">Bank 3</option>
-                    {/* Add more options as needed */}
-                  </select>
-                </div>
-              </div>
-              <div className="grid border-b py-4 grid-cols-1 md:grid-cols-12">
-                <div className="col-span-12 md:col-span-4 lg:col-span-3">
-                  <div className="flex">
-                    <label htmlFor="Title" className="font-semibold">
-                      Branch Name/Code
-                    </label>
-                  </div>
-                </div>
-                <div className="col-span-12 md:col-span-8 lg:col-span-6">
-                  <input
-                    type="text"
-                    id="Title"
-                    placeholder="Enter"
-                    className="myinput focus-none w-full"
-                  />
-                </div>
-              </div>
-              <div className="grid border-b py-4 grid-cols-1 md:grid-cols-12">
-                <div className="col-span-12 md:col-span-4 lg:col-span-3">
-                  <div className="flex">
-                    <label htmlFor="Title" className="font-semibold">
-                      Account Number*
-                    </label>
-                  </div>
-                </div>
-                <div className="col-span-12 md:col-span-8 lg:col-span-6">
-                  <input
-                    type="text"
-                    id="Title"
-                    placeholder="Enter here..."
-                    className="myinput focus-none w-full"
-                  />
-                </div>
-              </div>
-              <div className="grid py-4 grid-cols-1 md:grid-cols-12">
-                <div className="col-span-12 md:col-span-4 lg:col-span-3">
-                  <div className="flex">
-                    <label htmlFor="Title" className="font-semibold">
-                      Bank Routing Number*
-                    </label>
-                  </div>
-                </div>
-                <div className="col-span-12 md:col-span-8 lg:col-span-6">
-                  <input
-                    type="text"
-                    id="Title"
-                    placeholder="Enter here..."
-                    className="myinput focus-none w-full"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="flex justify-end mt-4">
-              <button
-                type="reset"
-                className="border border-[#cdcdcd] rounded-lg w-[150px] py-[10px] me-4 font-semibold bg-[#ffffff]"
-              >
-                {" "}
-                Cancel
-              </button>
-              <button
-                type="submit"
-                className="border border-[#0F91D2] rounded-lg w-[150px] py-[10px] text-[#ffffff] font-semibold bg-[#0F91D2]"
-              >
-                Save
-              </button>
-            </div>
-          </div>
-        </TabPanel>
-        <TabPanel value={value} index={5}>
-          <div>
-            <div className="border-b border-[#E9EAEB] pb-5 items-center flex-wrap gap-4">
-              <p className="text-lg font-semibold text-[#181D27]">
                 Additional Photos
               </p>
               <p className="text-[#535862] text-sm">
@@ -1042,7 +925,7 @@ function Settings() {
             </div>
           </div>
         </TabPanel>
-        <TabPanel value={value} index={6}>
+        <TabPanel value={value} index={5}>
           <div>
             <div className="border-b border-[#E9EAEB] pb-5 items-center flex-wrap gap-4">
               <p className="text-lg font-semibold text-[#181D27]">
@@ -1163,7 +1046,7 @@ function Settings() {
             </div>
           </div>
         </TabPanel>
-        <TabPanel value={value} index={7}>
+        <TabPanel value={value} index={6}>
           <div>
             <div className="border-b border-[#E9EAEB] pb-5 items-center flex-wrap gap-4">
               <p className="text-lg font-semibold text-[#181D27]">Password</p>
@@ -1244,7 +1127,7 @@ function Settings() {
             </div>
           </div>
         </TabPanel>
-        <TabPanel value={value} index={8}>
+        <TabPanel value={value} index={7}>
           <div>
             <div className="border-b border-[#E9EAEB] pb-5 items-center flex-wrap gap-4">
               <p className="text-lg font-semibold text-[#181D27]">
@@ -1430,6 +1313,123 @@ function Settings() {
                     type="text"
                     id="Title"
                     placeholder="Enter contact here"
+                    className="myinput focus-none w-full"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-end mt-4">
+              <button
+                type="reset"
+                className="border border-[#cdcdcd] rounded-lg w-[150px] py-[10px] me-4 font-semibold bg-[#ffffff]"
+              >
+                {" "}
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="border border-[#0F91D2] rounded-lg w-[150px] py-[10px] text-[#ffffff] font-semibold bg-[#0F91D2]"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel value={value} index={8}>
+          <div>
+            <div className="border-b border-[#E9EAEB] pb-5 items-center flex-wrap gap-4">
+              <p className="text-lg font-semibold text-[#181D27]">
+                Payment/Payout Info
+              </p>
+              <p className="text-[#535862] text-sm">
+                update your payment detail
+              </p>
+            </div>
+            <div className="">
+              <div className="grid border-b py-4 grid-cols-1 md:grid-cols-12">
+                <div className="col-span-12 md:col-span-4 lg:col-span-3">
+                  <div className="flex">
+                    <label htmlFor="Title" className="font-semibold">
+                      Service Title
+                    </label>
+                  </div>
+                </div>
+                <div className="col-span-12 md:col-span-8 lg:col-span-6">
+                  <input
+                    type="text"
+                    id="Title"
+                    placeholder="Muhammad Hussnain"
+                    className="myinput focus-none w-full"
+                  />
+                </div>
+              </div>
+              <div className="grid border-b py-4 grid-cols-1 md:grid-cols-12">
+                <div className="col-span-12 md:col-span-4 lg:col-span-3">
+                  <div className="flex">
+                    <label htmlFor="Title" className="font-semibold">
+                      Select Bank*
+                    </label>
+                  </div>
+                </div>
+                <div className="col-span-12 md:col-span-8 lg:col-span-6">
+                  <select id="Bank" className="myinput focus-none w-full">
+                    <option value="" disabled selected hidden>
+                      Select a bank
+                    </option>
+                    <option value="bank1">Bank 1</option>
+                    <option value="bank2">Bank 2</option>
+                    <option value="bank3">Bank 3</option>
+                    {/* Add more options as needed */}
+                  </select>
+                </div>
+              </div>
+              <div className="grid border-b py-4 grid-cols-1 md:grid-cols-12">
+                <div className="col-span-12 md:col-span-4 lg:col-span-3">
+                  <div className="flex">
+                    <label htmlFor="Title" className="font-semibold">
+                      Branch Name/Code
+                    </label>
+                  </div>
+                </div>
+                <div className="col-span-12 md:col-span-8 lg:col-span-6">
+                  <input
+                    type="text"
+                    id="Title"
+                    placeholder="Enter"
+                    className="myinput focus-none w-full"
+                  />
+                </div>
+              </div>
+              <div className="grid border-b py-4 grid-cols-1 md:grid-cols-12">
+                <div className="col-span-12 md:col-span-4 lg:col-span-3">
+                  <div className="flex">
+                    <label htmlFor="Title" className="font-semibold">
+                      Account Number*
+                    </label>
+                  </div>
+                </div>
+                <div className="col-span-12 md:col-span-8 lg:col-span-6">
+                  <input
+                    type="text"
+                    id="Title"
+                    placeholder="Enter here..."
+                    className="myinput focus-none w-full"
+                  />
+                </div>
+              </div>
+              <div className="grid py-4 grid-cols-1 md:grid-cols-12">
+                <div className="col-span-12 md:col-span-4 lg:col-span-3">
+                  <div className="flex">
+                    <label htmlFor="Title" className="font-semibold">
+                      Bank Routing Number*
+                    </label>
+                  </div>
+                </div>
+                <div className="col-span-12 md:col-span-8 lg:col-span-6">
+                  <input
+                    type="text"
+                    id="Title"
+                    placeholder="Enter here..."
                     className="myinput focus-none w-full"
                   />
                 </div>
