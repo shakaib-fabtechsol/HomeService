15f3cfe7f0e36668c88db1a451bd8f97304582ce
saim-fabtechsol <saim.ishfaq@fabtechsol.com>
2025-01-30 13:13:00 +0500

    service details

--- src/Layouts/Admin/ServiceDetail.jsx
+++ src/Layouts/Admin/ServiceDetail.jsx
@@ -1,10 +1,20 @@
 import React, { useEffect } from "react";
 import { FaArrowLeft, FaRegTrashCan } from "react-icons/fa6";
-import { FaPencilAlt } from "react-icons/fa";
-import { Link } from "react-router";
+import { FaPencilAlt, FaRegCalendarAlt } from "react-icons/fa";
+import { Link, useNavigate } from "react-router";
 import servicedet from "../../assets/img/service-det.png";
-import { Box, Tab, Tabs, TabScrollButton } from "@mui/material";
+import { Box, Modal, Tab, Tabs, TabScrollButton } from "@mui/material";
 import PropTypes from "prop-types";
+import {
+  IoChatbubbleEllipsesOutline,
+  IoLocationOutline,
+} from "react-icons/io5";
+import { IoIosStar } from "react-icons/io";
+import provider from "../../assets/img/provider.png";
+import { BiMessageAltDetail, BiMessageSquareDetail } from "react-icons/bi";
+import { FiPhone } from "react-icons/fi";
+import { TbMailDown } from "react-icons/tb";
+import { PiChats } from "react-icons/pi";
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -46,6 +56,33 @@ function ServiceDetail() {
     setValue(newValue);
   };
 
+  const [contactopen, setcontactOpen] = React.useState(false);
+  const handlecontactOpen = () => setcontactOpen(true);
+  const handlecontactClose = () => setcontactOpen(false);
+
+  const modalContacts = [
+    { path: "#", Icon: <FiPhone />, title: "Call Pro: (785) 712-6532" },
+    {
+      path: "#",
+      Icon: <BiMessageSquareDetail />,
+      title: "Text Pro: (708) 813-8989",
+    },
+    {
+      path: "#",
+      Icon: <BiMessageAltDetail />,
+      title: "Instant Chat",
+    },
+    { path: "#", Icon: <TbMailDown />, title: "Email Pro" },
+    { path: "#", Icon: <PiChats />, title: "Direct Form" },
+    {
+      path: "#",
+      Icon: <IoLocationOutline />,
+      title: "Get Directions",
+    },
+  ];
+
+  const navigate = useNavigate();
+
   return (
     <div>
       <div className="flex items-center">
@@ -78,88 +115,161 @@ function ServiceDetail() {
       </div>
       <div className="grid mt-4 grid-cols-1 md:grid-cols-12 gap-4">
         <div className="col-span-12 xl:col-span-8">
+          <div className="">
+            <div className="flex flex-wrap items-center">
+              <img
+                onClick={() => navigate("/ProfileDetails")}
+                src={provider}
+                alt=""
+                className="me-2 my-2 rounded-lg max-w-[120px] cursor-pointer"
+              />
+              <div className="my-2">
+                <div className="flex">
+                  <p className="font-semibold myhead me-2">Provider Name</p>
+                  <div className="flex">
+                    <IoIosStar className="me-2 text-[#F8C600]" />
+                    <p className="myblack text-sm">
+                      <span className="myhead font-semibold">4.9</span>(457)
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-wrap mt-2">
+                  <p className="myblack pe-3 me-3 border-e">House Cleaning</p>
+                  <div className="flex items-center">
+                    <IoLocationOutline className="me-2 myblack" />
+                    <p className="myblack ">Address of the provider here</p>
+                  </div>
+                </div>
+                <div className="flex mt-2">
+                  <div className="flex me-2">
+                    <FaRegCalendarAlt className="me-2" />
+                    <p className="text-sm myblack">Hours:</p>
+                    <p className="text-sm text-[#34A853]"> Available</p>
+                  </div>
+                  <p className="text-sm myblack">Close 6PM</p>
+                </div>
+              </div>
+            </div>
+            <Modal
+              open={contactopen}
+              onClose={handlecontactClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+              sx={{ m: 2 }}
+            >
+              <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[400px] outline-none">
+                <div className="bg-white rounded-[12px] p-4 max-h-[calc(100dvh-200px)] overflow-y-auto scroll-x-hidden">
+                  <p className="text-lg font-semibold">Contact Pro</p>
+                  <div className="flex flex-col gap-3 mt-4">
+                    {modalContacts.map((contact, index) => (
+                      <Link
+                        key={index}
+                        className="bg-[#FB8803] text-white flex items-center justify-center gap-2 p-3 rounded-[8px] text-sm font-medium"
+                        to={contact.path}
+                      >
+                        <span className="text-[24px]">{contact.Icon}</span>
+                        <span>{contact.title}</span>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </Modal>
+          </div>
           <img src={servicedet} alt="" className="rounded-xl w-full" />
         </div>
-        <div className="col-span-12 bg-[#FAFAFA] xl:col-span-4 border rounded-lg">
-          <div className="py-5 lg:px-6 px-4">
-            <Box sx={{ width: "100%" }}>
-              <Box
-                sx={{
-                  border: "1px solid #E9EAEB",
-                  borderRadius: "12px",
-                  overflow: "hidden",
-                }}
-              >
-                <Tabs
-                  value={value}
-                  onChange={handleChange}
-                  aria-label="basic tabs example"
-                  variant="scrollable"
-                  TabIndicatorProps={{ sx: { display: "none" } }}
+        <div className="col-span-12 xl:col-span-4">
+          <div className="flex flex-col h-full gap-5">
+            <div className="py-5 bg-[#FAFAFA] h-full border rounded-lg lg:px-6 px-4">
+              <Box sx={{ width: "100%" }}>
+                <Box
                   sx={{
-                    backgroundColor: "#ffffff",
-                    "& .MuiTab-root": {
-                      color: "#535862",
-                      textTransform: "capitalize",
-                      fontFamily: "inter",
-                    },
-                    "& .Mui-selected": { color: "#181D27", fontWeight: "700" },
+                    border: "1px solid #E9EAEB",
+                    borderRadius: "12px",
+                    overflow: "hidden",
                   }}
                 >
-                  <Tab label="Basic" {...a11yProps(0)} />
-                  <Tab label="Standard" {...a11yProps(1)} />
-                  <Tab label="Premium" {...a11yProps(2)} />
-                </Tabs>
+                  <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="basic tabs example"
+                    variant="scrollable"
+                    TabIndicatorProps={{ sx: { display: "none" } }}
+                    sx={{
+                      backgroundColor: "#ffffff",
+                      "& .MuiTab-root": {
+                        color: "#535862",
+                        textTransform: "capitalize",
+                        fontFamily: "inter",
+                      },
+                      "& .Mui-selected": {
+                        color: "#181D27",
+                        fontWeight: "700",
+                      },
+                    }}
+                  >
+                    <Tab label="Basic" {...a11yProps(0)} />
+                    <Tab label="Standard" {...a11yProps(1)} />
+                    <Tab label="Premium" {...a11yProps(2)} />
+                  </Tabs>
+                </Box>
+                <CustomTabPanel value={value} index={0}>
+                  <div className="flex justify-between">
+                    <h2 className="text-2xl font-medium myhead">Plan Title</h2>
+                    <p className="text-3xl myhead font-bold">$200</p>
+                  </div>
+                  <p className="text-sm myblack mt-2">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Morbi tellus diam, dignissim tincidunt quam vel, rutrum
+                    egestas lacus. Phasellus accumsan fermentum dolor eu
+                    gravida. Vivamus dignissim augue sed orci interdum vehicula.
+                  </p>
+                  <ul className="mt-4 myblack text-sm list-disc space-y-1 pl-5">
+                    <li>3 Workers</li>
+                    <li>Delivered Within 2 Days</li>
+                  </ul>
+                </CustomTabPanel>
+                <CustomTabPanel value={value} index={1}>
+                  <div className="flex justify-between">
+                    <h2 className="text-2xl font-medium myhead">Plan Title</h2>
+                    <p className="text-3xl myhead font-bold">$400</p>
+                  </div>
+                  <p className="text-sm myblack mt-2">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Morbi tellus diam, dignissim tincidunt quam vel, rutrum
+                    egestas lacus. Phasellus accumsan fermentum dolor eu
+                    gravida. Vivamus dignissim augue sed orci interdum vehicula.
+                  </p>
+                  <ul className="mt-4 myblack text-sm list-disc space-y-1 pl-5">
+                    <li>3 Workers</li>
+                    <li>Delivered Within 2 Days</li>
+                  </ul>
+                </CustomTabPanel>
+                <CustomTabPanel value={value} index={2}>
+                  <div className="flex justify-between">
+                    <h2 className="text-2xl font-medium myhead">Plan Title</h2>
+                    <p className="text-3xl myhead font-bold">$600</p>
+                  </div>
+                  <p className="text-sm myblack mt-2">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Morbi tellus diam, dignissim tincidunt quam vel, rutrum
+                    egestas lacus. Phasellus accumsan fermentum dolor eu
+                    gravida. Vivamus dignissim augue sed orci interdum vehicula.
+                  </p>
+                  <ul className="mt-4 myblack text-sm list-disc space-y-1 pl-5">
+                    <li>3 Workers</li>
+                    <li>Delivered Within 2 Days</li>
+                  </ul>
+                </CustomTabPanel>
               </Box>
-              <CustomTabPanel value={value} index={0}>
-                <div className="flex justify-between">
-                  <h2 className="text-2xl font-medium myhead">Plan Title</h2>
-                  <p className="text-3xl myhead font-bold">$200</p>
-                </div>
-                <p className="text-sm myblack mt-2">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-                  tellus diam, dignissim tincidunt quam vel, rutrum egestas
-                  lacus. Phasellus accumsan fermentum dolor eu gravida. Vivamus
-                  dignissim augue sed orci interdum vehicula.
-                </p>
-                <ul className="mt-4 myblack text-sm list-disc space-y-1 pl-5">
-                  <li>3 Workers</li>
-                  <li>Delivered Within 2 Days</li>
-                </ul>
-              </CustomTabPanel>
-              <CustomTabPanel value={value} index={1}>
-                <div className="flex justify-between">
-                  <h2 className="text-2xl font-medium myhead">Plan Title</h2>
-                  <p className="text-3xl myhead font-bold">$400</p>
-                </div>
-                <p className="text-sm myblack mt-2">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-                  tellus diam, dignissim tincidunt quam vel, rutrum egestas
-                  lacus. Phasellus accumsan fermentum dolor eu gravida. Vivamus
-                  dignissim augue sed orci interdum vehicula.
-                </p>
-                <ul className="mt-4 myblack text-sm list-disc space-y-1 pl-5">
-                  <li>3 Workers</li>
-                  <li>Delivered Within 2 Days</li>
-                </ul>
-              </CustomTabPanel>
-              <CustomTabPanel value={value} index={2}>
-                <div className="flex justify-between">
-                  <h2 className="text-2xl font-medium myhead">Plan Title</h2>
-                  <p className="text-3xl myhead font-bold">$600</p>
-                </div>
-                <p className="text-sm myblack mt-2">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-                  tellus diam, dignissim tincidunt quam vel, rutrum egestas
-                  lacus. Phasellus accumsan fermentum dolor eu gravida. Vivamus
-                  dignissim augue sed orci interdum vehicula.
-                </p>
-                <ul className="mt-4 myblack text-sm list-disc space-y-1 pl-5">
-                  <li>3 Workers</li>
-                  <li>Delivered Within 2 Days</li>
-                </ul>
-              </CustomTabPanel>
-            </Box>
+            </div>
+            <button
+              onClick={handlecontactOpen}
+              className="flex mt-3 lg:mt-0 py-3 justify-center items-center px-6 font-semibold rounded-lg text-[#fff] bg-[#FB8803]"
+            >
+              <IoChatbubbleEllipsesOutline className="me-2 text-[#fff] text-xl" />
+              <span>Contact Pro</span>
+            </button>
           </div>
         </div>
       </div>
